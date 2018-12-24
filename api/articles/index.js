4b1300e377560f3717c713c42df1b4585c5bb1dd
Phil Ngo <ngo.phil@gmail.com>
2018-12-24 13:53:41 -0800

    Add CORS header for development

--- api/articles/index.js
+++ api/articles/index.js
@@ -5,7 +5,10 @@ Eventualy swap this with a DB connection and better API.
 const data = require('./data.json')
 
 module.exports = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
   res.write(JSON.stringify(data))
   res.end()
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,6 @@
 export function callApiArticles () {
-  if (process.env.NODE_ENV === 'development') {
-    return new Promise((resolve) => {
-      resolve([
-        {
-          id: 1,
-          title: 'First post',
-          slug: 'first-post',
-          source: '/api/articles/a1.md',
-          published: '2018-12-24',
-          updated: '2018-12-24'
-        }
-      ])
-    })
-  } else {
-    return fetch(`/api/articles`).then(response => {
-      return response.json()
-    })
-  }
+  return fetch(`/api/articles`).then(response => {
+    return response.json()
+  })
 }
 
