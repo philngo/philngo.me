e6585922c683add9a163b82cfc337962129b0dec
Phil Ngo <ngo.phil@gmail.com>
2018-12-24 00:04:58 -0800

    Use https library instead of fetch

--- api/articles/index.js
+++ api/articles/index.js
@@ -2,13 +2,13 @@
 This is really just for rapid prototyping.
 Eventualy swap this with a DB connection and better API.
 */
-const fetch = require('node-fetch')
+const https = require('https')
 
 const data = require('./data.json').map(d => {
-  fetch(d.source).then(response => {
-    response.text().then(text => {
-      d.content = text
-    })
+  https.get(d.source, (response) => {
+    let data = ''
+    resp.on('data', (chunk) => { data += chunk })
+    resp.on('end', () => { d.content = data })
   })
 })
 
