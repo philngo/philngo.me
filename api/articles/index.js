03bd44d9da5569e3ab6e6c49fa31d4a2eefb9163
Phil Ngo <ngo.phil@gmail.com>
2018-12-23 22:48:57 -0800

    Read local file

--- api/articles/index.js
+++ api/articles/index.js
@@ -1,5 +1,7 @@
+const fs = require('fs')
+
 module.exports = (req, res) => {
-  const data = { test: "data" }
+  const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'))
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.write(JSON.stringify(data))
   res.end()
