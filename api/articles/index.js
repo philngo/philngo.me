82326c3dc899688e41c289aa7e4b66f45c21185c
Phil Ngo <ngo.phil@gmail.com>
2018-12-23 22:55:34 -0800

    Require data.json

--- api/articles/index.js
+++ api/articles/index.js
@@ -1,7 +1,6 @@
-const fs = require('fs')
+var data = require('./data.json')
 
 module.exports = (req, res) => {
-  const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'))
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.write(JSON.stringify(data))
   res.end()
