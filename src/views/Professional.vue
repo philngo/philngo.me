7e6a29385cb5457204a58a968476242ff5e59514
Phil Ngo <ngo.phil@gmail.com>
2018-12-30 16:13:56 -0800

    Update professional section

--- src/views/Professional.vue
+++ src/views/Professional.vue
@@ -2,13 +2,20 @@
   <div class="professional">
     <h2>Professional</h2>
     <p>
-      <i>This section is still under construction.</i>
+      I'm the author of the
+      <a href="http://eemeter.openee.io/" class="red">eemeter</a>
+      python package for computing the avoided energy use following
+      building energy efficiency projects, and the accompanying
+      <a href="https://eeweather.openee.io/" class="red">eeweather</a>
+      python package for obtaining dry bulb temperature data from various public sources.
     </p>
     <p>
-      I work at <a href="https://openee.io/#team">OpenEE</a>.
+      Find me on <a href="https://github.com/philngo/" class="red">github</a> or on
+      <a href="https://www.linkedin.com/in/phil-ngo-9b2b3b65/" class="red">LinkedIn</a>.
     </p>
     <p>
-      <a href="https://www.linkedin.com/in/phil-ngo-9b2b3b65/">LinkedIn</a>
+      The code for this website is
+      <a href="https://github.com/philngo/philngo.me" class="red">on github</a>.
     </p>
   </div>
 </template>
