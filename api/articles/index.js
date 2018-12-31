f874269ea9cb92ddac708732d3ab9fa3f178cf7f
Phil Ngo <ngo.phil@gmail.com>
2018-12-30 19:48:30 -0800

    Don't use writeHead for lambda

--- api/articles/index.js
+++ api/articles/index.js
@@ -4,11 +4,14 @@ Eventualy swap this with a DB connection and better API.
 */
 const data = require('./data.json')
 
+/*
+Can't use writeHead here, see the following:
+https://github.com/zeit/now-cli/issues/1670#issuecomment-440753564
+*/
 module.exports = (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-  })
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.write(JSON.stringify(data))
   res.end()
 }
