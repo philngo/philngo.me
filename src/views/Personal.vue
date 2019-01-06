89bec504c090a40899430b10039c2d7cc6a7c4f3
Phil Ngo <ngo.phil@gmail.com>
2019-01-06 15:29:16 -0800

    Small edits

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -2,8 +2,17 @@
   <div class="personal">
     <h2>Personal</h2>
     <p>
-      <i>This section is still under construction.</i>
-      (Not a joke about my personal life.)
+      I love learning new things. If such a major existed, I would have majored
+      in 101 classes.
+    </p>
+    <p>
+      Right now I'm trying to learn
+      <ul>
+        <li>How to ferment foods</li>
+        <li>How to produce content instead of consuming content</li>
+      </ul>
+    </p>
+    <p>
     </p>
   </div>
 </template>
